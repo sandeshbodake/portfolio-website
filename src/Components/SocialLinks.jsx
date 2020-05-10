11fb8285163163eb57bc7bab1b1e6767badfa24d
Sandesh Bodake <sandeshbodake@Sandeshs-MacBook-Pro.local>
2020-05-11 00:43:00 +0530

    linkedin social link added

--- src/Components/SocialLinks.jsx
+++ src/Components/SocialLinks.jsx
@@ -3,14 +3,15 @@ import React from 'react';
 const SocialLinks = props => {
   return (
     <div className="social">
-      <a
-        href="https://twitter.com/BodakeSandesh"
+       <a
+        id="profile-link"
+        href="https://www.linkedin.com/in/sandesh-bodake-2a8723153/"
         target="_blank"
         rel="noopener noreferrer"
-        title="Link to author's Twitter profile"
+        title="Link to author's Linkedin Profile"
       >
         {' '}
-        <i className="fab fa-twitter" />
+        <i className="fab fa-linkedin-in" />
       </a>
       <a
         id="profile-link"
@@ -41,6 +42,15 @@ const SocialLinks = props => {
       >
         {' '}
         <i className="fab fa-facebook" />
+      </a>
+      <a
+        href="https://twitter.com/BodakeSandesh"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="Link to author's Twitter profile"
+      >
+        {' '}
+        <i className="fab fa-twitter" />
       </a>
     </div>
   );
